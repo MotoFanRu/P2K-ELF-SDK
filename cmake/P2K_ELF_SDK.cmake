a8c4bb9ea8a0e7c1ceec361c658757a5ec0390d5
EXL <exlmotodev@gmail.com>
2023-11-28 11:37:06 +0700

    Import commit: 28-Nov-2023, 11:37:06 +0700

--- cmake/P2K_ELF_SDK.cmake
+++ cmake/P2K_ELF_SDK.cmake
@@ -1,16 +1,20 @@
 cmake_minimum_required(VERSION 3.16)
 
-set(ELFPACK "EP1" CACHE STRING "Default value for ELFPACK" FORCE)
-set(CPU_MODE "thumb" CACHE STRING "Default value for CPU_MODE" FORCE)
+set(ELFPACK "EP1" CACHE STRING "Default value for ELFPACK")
+set(CPU_MODE "thumb" CACHE STRING "Default value for CPU_MODE")
 
 set(P2K_TOOLCHAIN_ROOT "${CMAKE_CURRENT_LIST_DIR}/..")
 
+message(STATUS "Note: Will build for ELFPACK=${ELFPACK}")
+
 if(ELFPACK STREQUAL "EP1") # ElfPack v1.x, ARM/Thumb.
-	include("${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EP1_Toolchain.cmake")
+	message(STATUS "Note: Will build for CPU_MODE=${CPU_MODE}")
+	set(CMAKE_TOOLCHAIN_FILE "${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EP1_Toolchain.cmake")
 elseif(ELFPACK STREQUAL "EP2") # ElfPack v2.x, ARM/Thumb.
-	include("${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EP2_Toolchain.cmake")
+	message(STATUS "Note: Will build for CPU_MODE=${CPU_MODE}")
+	set(CMAKE_TOOLCHAIN_FILE "${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EP2_Toolchain.cmake")
 elseif(ELFPACK STREQUAL "EM1") # ElfPack v1.x, M*CORE.
-	include("${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EM1_Toolchain.cmake")
+	set(CMAKE_TOOLCHAIN_FILE "${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EM1_Toolchain.cmake")
 elseif(ELFPACK STREQUAL "EM2") # ElfPack v2.x, M*CORE.
-	include("${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EM2_Toolchain.cmake")
+	set(CMAKE_TOOLCHAIN_FILE "${P2K_TOOLCHAIN_ROOT}/cmake/P2K_EM2_Toolchain.cmake")
 endif()
